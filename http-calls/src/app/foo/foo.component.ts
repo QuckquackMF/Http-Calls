86e38f2e9bca1d9ffdb772a94beff50b77a309ba
QuckquackMF <toci.iljas@iisgalvanimi.edu.it>
2025-05-07 12:57:52 +0000

    http call

---
+++ http-calls/src/app/foo/foo.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import { OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Component({
+  selector: 'app-foo',
+  standalone: true,
+  imports: [],
+  templateUrl: './foo.component.html',
+  styleUrl: './foo.component.css'
+})
+export class FooComponent {
+data!: Object; //Il ‘!’ serve a creare variabili non inizializzate
+loading: boolean=false;
+o! :Observable<Object>;
+constructor(public http: HttpClient) {}
+makeRequest(): void {
+  console.log("here");
+  this.loading = true;
+  this.o = this.http.get('https://jsonplaceholder.typicode.com/posts/1');
+  this.o.subscribe(this.getData);
+}
+getData = (d : Object) =>
+{
+  this.data = new Object(d);
+  this.loading = false;
+}
+}
